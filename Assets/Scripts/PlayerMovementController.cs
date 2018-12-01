a396d334231b37ace140d49dcd691ef23a14d2e2
Benjamin Frye <ben.j.frye@gmail.com>
2018-12-01 14:02:10 +1100

    Player movement

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovementController : MonoBehaviour
+{
+    [Range(.5f, 15f)]
+    public float speed = 1f;
+    private Rigidbody2D myRigidbody2D;
+
+    void Start()
+    {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 delta = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        delta *= speed * Time.fixedDeltaTime;
+        Vector2 position = myRigidbody2D.position + delta;
+        myRigidbody2D.MovePosition(position);
+    }
+}
