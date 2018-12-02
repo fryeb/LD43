af3704706d264fb10bcb57eed6a2c4b2be0e8316
Benjamin Frye <ben.j.frye@gmail.com>
2018-12-03 08:51:24 +1100

    Fixed bug where standing on the spawn point would flood the world with enemies

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,17 +4,37 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public float boundingRadius = 1f;
+    public LayerMask layerMask;
     public Transform[] prefabs;
+
+    private Transform m_Transform;
+
     int spawnCount = 0;
+
+    void Start()
+    {
+        m_Transform = GetComponent<Transform>();
+    }
 
     void Update()
     {
         if (spawnCount > GameManager.Instance.killCount)
             return;
 
+        Collider2D hitCollider = Physics2D.OverlapCircle(m_Transform.position, boundingRadius, layerMask);
+        if (hitCollider != null)
+            return;
+
         spawnCount++;
 
         int index = Random.Range(0, prefabs.Length);
         Instantiate(prefabs[index], transform.position, Quaternion.identity);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, boundingRadius);
     }
 }
